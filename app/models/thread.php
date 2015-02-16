74bbe5b131646b165cb1c933745880987a845935
Adrianne Peredo <a.peredo@klab.com>
2015-02-16 16:12:43 +0800

    Can Add Comments

--- app/models/thread.php
+++ app/models/thread.php
@@ -39,4 +39,13 @@
 
             return $comments;
         }
+
+        public function write(Comment $comment)
+        {
+            $db = DB::conn();
+            $db->query(
+               'INSERT INTO comment SET thread_id = ?, username = ?, body = ?, created = NOW()',        
+             array($this->id, $comment->username, $comment->body)
+             );
+        }
     }
