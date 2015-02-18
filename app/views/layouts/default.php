b8f390c9fcc32f66bb4d813dcd9f7a1efcca3b20
Adrianne Peredo <a.peredo@klab.com>
2015-02-18 08:46:47 +0800

    Fix title and link to index

--- app/views/layouts/default.php
+++ app/views/layouts/default.php
@@ -2,7 +2,15 @@
 <html lang="en">
   <head>
     <meta charset="utf-8">
-    <title>DietCake <?php eh($title) ?></title>
+    <title>
+        Board
+        <?php
+          if (isset($thread)) {
+            echo " - ";
+            eh($thread->title);  
+          }
+        ?>
+      </title>
 
     <link href="/bootstrap/css/bootstrap.min.css" rel="stylesheet">
     <style>
@@ -17,7 +25,7 @@
     <div class="navbar navbar-fixed-top">
       <div class="navbar-inner">
         <div class="container">
-          <a class="brand" href="#">DietCake Hello</a>
+          <a class="brand" href="<?php eh(url(' thread/index')) ?>">DietCake Board</a>
         </div>
       </div>
     </div>
